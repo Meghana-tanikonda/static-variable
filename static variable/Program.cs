8a83d4b90e24fe8886a52cc48d47f09ec29d0c7b
megha <megha@VIJAY>
2025-04-04 12:54:01 +0530

    Add project files.

---
+++ static variable/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace MyNamespace
+{
+    public class MyClass
+    {
+        public static int a = 1;//static variable
+        public void variable()
+        {
+            a = a + 1;
+            Console.WriteLine($"Value of a is {a}");
+        }
+        static void Main(string[] args)
+        {
+            MyClass var1 = new MyClass();
+            var1.variable();
+            MyClass var2 = new MyClass();
+            var2.variable();
+            MyClass var3 = new MyClass();
+            var3.variable();
+            Console.ReadKey();
+
+        }
+    }
+}
